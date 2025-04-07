dc59ede9018eb712f899684b41a2a361edb1dc1b
Anna Mieczyńska <UL0262881@edu.uni.lodz.pl>
2025-04-07 16:48:58 +0200

    Dodaj pliki projektów.

---
+++ AdminMenuForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SystemObslugiPrzychodni
+{
+    partial class AdminMenuForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            OpenAddUserFormButton = new Button();
+            OpenUserListFormButton = new Button();
+            SuspendLayout();
+            // 
+            // OpenAddUserFormButton
+            // 
+            OpenAddUserFormButton.Location = new Point(376, 241);
+            OpenAddUserFormButton.Name = "OpenAddUserFormButton";
+            OpenAddUserFormButton.Size = new Size(192, 41);
+            OpenAddUserFormButton.TabIndex = 1;
+            OpenAddUserFormButton.Text = "Dodaj użytkownika";
+            OpenAddUserFormButton.UseVisualStyleBackColor = true;
+            OpenAddUserFormButton.Click += OpenAddUserFormButton_Click;
+            // 
+            // OpenUserListFormButton
+            // 
+            OpenUserListFormButton.Location = new Point(376, 298);
+            OpenUserListFormButton.Name = "OpenUserListFormButton";
+            OpenUserListFormButton.Size = new Size(192, 36);
+            OpenUserListFormButton.TabIndex = 2;
+            OpenUserListFormButton.Text = "Lista użytkowników";
+            OpenUserListFormButton.UseVisualStyleBackColor = true;
+            OpenUserListFormButton.Click += OpenUserListFormButton_Click;
+            // 
+            // AdminMenuForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1068, 571);
+            Controls.Add(OpenUserListFormButton);
+            Controls.Add(OpenAddUserFormButton);
+            Name = "AdminMenuForm";
+            Text = "Menu Administracyjne";
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button OpenAddUserFormButton;
+        private Button OpenUserListFormButton;
+    }
+}
---
+++ UserListForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemObslugiPrzychodni
+{
+    public partial class UserListForm : Form
+    {
+        public UserListForm()
+        {
+            InitializeComponent();
+            RefreshUserList();
+        }
+
+        public void RefreshUserList()
+        {
+            dataGridViewUserList.DataSource = null;
+            dataGridViewUserList.DataSource = UserManagement.GetAllUsers();
+            HideData();
+        }
+
+        public void HideData()
+        {
+            dataGridViewUserList.Columns["User_id"].Visible = false;
+            dataGridViewUserList.Columns["Password"].Visible = false;
+            dataGridViewUserList.Columns["City"].Visible = false;
+            dataGridViewUserList.Columns["Post_Code"].Visible = false;
+            dataGridViewUserList.Columns["Street"].Visible = false;
+            dataGridViewUserList.Columns["Street_number"].Visible = false;
+            dataGridViewUserList.Columns["Apartment_number"].Visible = false;
+            dataGridViewUserList.Columns["Street"].Visible = false;
+            dataGridViewUserList.Columns["Pesel"].Visible = false;
+            dataGridViewUserList.Columns["DateOfBirth"].Visible = false;
+            dataGridViewUserList.Columns["Sex"].Visible = false;
+            dataGridViewUserList.Columns["Phone"].Visible = false;
+            dataGridViewUserList.Columns["Role_id"].Visible = false;
+            dataGridViewUserList.Columns["Is_active"].Visible = false;
+        }
+
+        private void OpenAdminMenuFormButton_Click(object sender, EventArgs e)
+        {
+            AdminMenuForm form1 = new AdminMenuForm();
+            form1.Show();
+            this.Close();
+        }
+
+        private void FilterDataGridView()
+        {
+            string filterLogin = textBoxLogin.Text.Trim();
+            string filterSurname = textBoxSurname.Text.Trim();
+            string filterPesel = textBoxPESEL.Text.Trim();
+
+            var filteredUsers = UserManagement.users.Where(u =>
+                 (string.IsNullOrEmpty(filterLogin) || u.Login.Contains(filterLogin, StringComparison.OrdinalIgnoreCase)) &&
+                 (string.IsNullOrEmpty(filterSurname) || u.Surname.Contains(filterSurname, StringComparison.OrdinalIgnoreCase)) &&
+                 (string.IsNullOrEmpty(filterPesel) || u.Pesel.Contains(filterPesel))
+            ).ToList();
+
+            dataGridViewUserList.DataSource = null;
+            dataGridViewUserList.DataSource = filteredUsers;
+            HideData();
+        }
+
+        private void SearchUserButton_Click(object sender, EventArgs e)
+        {
+            FilterDataGridView();
+        }
+
+        private void ResetViewButton_Click(object sender, EventArgs e)
+        {
+            RefreshUserList();
+        }
+
+        private void OpenUserDetailsForm_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewUserList.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridViewUserList.SelectedRows[0];
+
+                User selectedUser = selectedRow.DataBoundItem as User;
+
+                if (selectedUser != null)
+                {
+                    UserDetailsForm editForm = new UserDetailsForm(selectedUser);
+                    editForm.Show();
+                    this.Close();
+                    //RefreshUserList();
+                }
+                else
+                {
+                    MessageBox.Show("Nie udało się pobrać danych użytkownika.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano użytkownika.");
+            }
+        }
+    }
+}
